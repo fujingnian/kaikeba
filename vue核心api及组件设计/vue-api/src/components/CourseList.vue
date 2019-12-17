d49fd07698c0ba371b64756ad1d4c05ba9973ca7
fuyan <fuyan@jze100.com>
2019-12-17 18:15:33 +0800

    feat: 新增购物车布局

---
+++ vue核心api及组件设计/vue-api/src/components/CourseList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div calss="course-list">
+    <h1>开课吧 - 购物车</h1>
+    <hr />
+    <div class="list">
+      <h2>添加课程</h2>
+      <div>
+        课程名称：
+        <input type="text" v-model="name" />
+      </div>
+      <div>
+        课程价格：
+        <input type="text" v-model="price" />
+      </div>
+      <div>
+        <button @click="handleSubmit">添加课程到列表</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CourseList",
+  data() {
+    return {
+      name: "",
+      price: ""
+    };
+  },
+  mounted() {},
+  methods: {
+    handleSubmit() {
+      console.log(this.name);
+    }
+  }
+};
+</script>
---
+++ vue核心api及组件设计/vue-api/src/views/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <course-list></course-list>
+  </div>
+</template>
+
+<script>
+import CourseList from "@/components/CourseList";
+export default {
+  name: "cart",
+  components: {
+    CourseList
+  },
+  data() {
+    return {};
+  },
+  mounted() {}
+};
+</script>
